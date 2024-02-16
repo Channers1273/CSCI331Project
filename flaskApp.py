b1a3537143cc76cdc390a530f7386072d3495161
Channers1273 <kykychan@outlook.com>
2024-02-15 17:19:25 -0800

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Jordan can't code without VSCode <h4>WHAT A GOOBER</h4>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run()
