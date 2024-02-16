2af5131010d2e604b2ea7593292461155abaab1a
115Soup <jbudd2004@gmail.com>
2024-02-15 16:23:49 -0800

    added friendsList page to jordansflaskapp

--- jordansflaskapp.py
+++ jordansflaskapp.py
@@ -2,10 +2,17 @@ from flask import Flask, redirect, render_template, url_for
 from initialPythonFunctions import SteamUser
 
 app = Flask(__name__)
+
+
 @app.route("/")
-def home():
+def index():
     name = YOU.username
     return f"Welcome {name}"
+
+@app.route("/friendsList")
+def friendsList():
+    flist = YOU.friendsList
+    return render_template("friendsList.html", name=YOU.username, friends=flist)
 
 
 
