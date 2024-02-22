4e760e6388267df9f7ce2a65b3c407849e990277
sing20236 <128087243+sing20236@users.noreply.github.com>
2024-02-21 22:47:44 -0800

    Create Login and Basic nav
    - Create login.html (IDs are listed below for ease of login)
    - Create base.html (all default nav bar, style setting, to be polished)
    - base.html (home and friends button to replace the ahref links)
    - welcome.html / friendsList.html extends base.html; remove ahref links
    - initialPythonFunctions.py comments out getRecentGames()
    - flaskApp.py adjusts YOU in main to global variable, allow input for YOU, return according to YOU value; app.run(debug=True) is convenient to save rerun server after modification

--- flaskApp.py
+++ flaskApp.py
@@ -1,25 +1,42 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request
 from initialPythonFunctions import *
 
 app = Flask(__name__)
 
+# Initial YOU as None such that index() return login at first
+YOU = None
 
 @app.route("/")
 def index():
-    return render_template("welcome.html", name=YOU.username)
+    # when YOU exists (with input), render welcome.html
+    # Error handling to be done for invalid Steam ID
+    if YOU:
+        return render_template("welcome.html", name=YOU.username)
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/friendsList")
 def friendsList():
     flist = YOU.friendsList
     return render_template("friendsList.html", name=YOU.username, friends=flist)
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    # declare YOU as global variable for storing the input value
+    global YOU
 
-
+    # post method is set in login.html
+    if request.method == "POST":
+        user_id = request.form["sid"]
+        YOU = SteamUser(user_id)
+        return redirect(url_for("index"))
+    else:
+        return render_template("login.html")    
 
 if __name__ == '__main__':
-    print("Please enter your steam id")
-    id = JordanSteamID
-    YOU = SteamUser(id)
+    # print("Please enter your steam id")
+    # id = JordanSteamID
+    # YOU = SteamUser(id)
 
-
-    app.run()
+    # debug is set to save rerun a server when change is made
+    app.run(debug=True)
--- initialPythonFunctions.py
+++ initialPythonFunctions.py
@@ -24,7 +24,7 @@ class SteamUser:
         self.steamID = steamID
         self.username = self.getUsername() 
         self.friendsList = self.getFriends()
-        self.recentGames = self.getRecentGames()
+        # self.recentGames = self.getRecentGames()
         self.achievements = {}
 
     def getUsername(self):
